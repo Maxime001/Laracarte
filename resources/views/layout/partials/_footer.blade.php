63e24bec94db1257c1fd8ce52a2469f57793c754
MaximeS <maxime.simet@yahoo.fr>
2017-05-30 14:15:27 +0200

    Setup and add static pages

---
+++ resources/views/layout/partials/_footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="text-center">
+    <p>&copy;{{ date('Y') }} &middot; Laracarte by <a href="https://twitter.com/etsmo">@etsmo</a> - A <a href="https://laramap.com">Laramap</a> clone app. </p>
+    <p><b>This app has benn built for learning purposes. </b></p>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+
+@section('content')
+    <h1>Laracarte</h1>
+@stop
